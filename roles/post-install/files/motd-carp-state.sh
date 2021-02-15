2dae2d1ae499d31060374ef657b66449f4926f12
Jeremy Dubois <jdubois@evolix.fr>
2021-02-15 18:56:24 +0100

    Fix typo

--- roles/post-install/files/motd-carp-state.sh
+++ roles/post-install/files/motd-carp-state.sh
@@ -5,7 +5,7 @@ if [ ! -f /etc/motd-original ]; then
 fi
 
 if [ ! -f /tmp/carp.state ]; then
-    # Raplace OpenBSD version in motd after each boot for it to be up to date after an upgrade
+    # Replace OpenBSD version in motd after each boot for it to be up to date after an upgrade
     sed -i "1 s/^.*$/$(head -1 \/var\/run\/dmesg.boot)/" /etc/motd-original
     echo "unknown" > /tmp/carp.state
 fi
